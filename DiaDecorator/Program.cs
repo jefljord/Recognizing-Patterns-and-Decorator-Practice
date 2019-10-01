f81f12382b4accf74ee84a01dfacf3a5d7b934b0
jeff jordan <jeffjordan33@outlook.com>
2019-10-01 10:35:35 -0500

    Add project files.

---
+++ DiaDecorator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface Widget
+    {
+        void draw();
+    }
+
+    class TextField : Widget
+    {
+        private int width;
+        private int height;
+
+        public TextField(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("Text field has been drawn.");
+        }
+    }
+
+    abstract class Decorator : Widget
+    {
+        private Widget wid;
+
+        public Decorator(Widget w)
+        {
+        }
+
+        public void draw()
+        {
+        }
+    }
+
+    class BorderDecorator : Widget
+    {
+        public BorderDecorator(Widget w)
+        {
+        }
+        public void draw()
+        {
+            Console.WriteLine("I am a border decorator holding a: ");
+        }
+    }
+
+    class ScrollDecorator : Widget
+    {
+        public ScrollDecorator(Widget w)
+        {
+
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("I am a scroll decorator holding a:");
+        }
+    }
+}
